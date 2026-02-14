27281bcabad9de421a7ac7c8a949f0226088ce1d
Brainy0789 <bpurdy12@icloud.com>
2026-02-13 16:13:58 -0800

    we are fixing things!!!

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -92,7 +92,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.data.checkForUpdates && !closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/ShadowMario/FNF-PsychEngine/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/Brainy0789/BrainyPsych/refs/heads/main/gitVersion.txt");
 
 			http.onData = function (data:String)
 			{
